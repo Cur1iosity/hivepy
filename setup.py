8e1a8b23129ee2f0fd8ada5ec74d88b9b753ba68
Cur1iosity <Ilyakovalevk@gmail.com>
2023-10-22 17:35:19 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,18 @@ setup(
    name='HivePy',
    version='0.1.0',
    description='Small wrapper for HexWay Hive API',
+   long_description=open('README.md').read(),
+   long_description_content_type='text/markdown',
    author='@Cur1iosity',
-   author_email='Ilyakovalevk@gmail.com',
+   author_email='Cur1iosity@protonmail.com',
+   url='https://github.com/Cur1iosity/hivepy',
    packages=find_packages(),
    install_requires=['requests>=2.31.0', 'pydantic>=2.4.0'],
+   classifiers=[
+      # Классификаторы для PyPI
+      'Programming Language :: Python :: 3',
+      'License :: OSI Approved :: MIT License',
+      'Operating System :: OS Independent',
+   ],
+   python_requires='>=3.6',
 )
