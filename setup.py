5e0db07fcf85f9f7541cd2e74be2001fb2d31bd5
Cur1iosity <Ilyakovalevk@gmail.com>
2023-10-22 18:17:27 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
    name='hw-hivepy',
-   version='0.1.0',
+   version='0.1.1',
    description='Small wrapper for HexWay Hive API',
    long_description=open('README.md').read(),
    long_description_content_type='text/markdown',
