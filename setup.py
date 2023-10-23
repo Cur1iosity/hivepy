ddd54e9aed98d4f1b4d90f7a9809ddede22de705
Cur1iosity <Ilyakovalevk@gmail.com>
2023-10-23 11:32:12 +0300

    Minor updates: - change mode for serialization to beware recursive dumping (for Issue)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
    name='hw-hivepy',
-   version='0.1.6',
+   version='0.1.7',
    description='Small wrapper for HexWay Hive API',
    long_description=open('README.md').read(),
    long_description_content_type='text/markdown',
